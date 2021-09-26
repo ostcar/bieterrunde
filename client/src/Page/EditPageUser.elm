9590d32ccd1214647532192ecce899b2f158585d
Stauberle <89799915+Stauberle@users.noreply.github.com>
2021-09-26 21:56:51 +0200

    neue ELM Datei EditPage Test

---
+++ client/src/Page/EditPageUser.elm
@@ -0,0 +1,27 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+
+
+
+        -- div[]
+        --     [
+        --         table[]
+        --         [
+        --             thead[]
+        --             [
+        --                 th [][ text "Product"],
+        --             ]
+        --             tr []
+        --             [
+        --                 td []
+        --                 [
+        --                     [text "Deine Bieternummer ist "
+        --                     , strong [] [ text (Bieter.idToString bieter.id) ]
+        --                     , text ". Merke sie dir gut. Du brauchst sie für die nächste Anmeldung",
+        --                     ]
+        --                 ]
+        --             ]                 
+        --         ]
+        --     ]
